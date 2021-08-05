27784a2cfe4dbff9eff1d00250f15ef33ae7de87
Chapaev Anton <virsus2009@gmail.com>
2021-08-05 21:06:03 +0700

    reworked unit class hierarchy
    * optimized method fight (CombatUnit attacker, CombatUnit defender)

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,21 @@ public class Main {
     public static void main(String[] args) {
         Merchant merch = new Merchant("Bob", 150, 2500, 0);
         System.out.println(merch);
-        Hero hero = new Hero("Дзюба", 1, 500, 50, 0, 0, 20, 20,
-                20, 10, 10);
-        hero.setLevel(7);
-        GeneratorUnits generatorUnits = new GeneratorUnits(hero);
-        List<Monster> listMonster = generatorUnits.generateMonsters();
+        System.out.println(merch.getListOfGood());
 
-        System.out.println(listMonster);
-        System.out.println(listMonster.size());
+//        Hero hero = new Hero("Дзюба", 1, 500, 50, 0, 0, 20, 20,
+//                20, 10, 10);
+//        hero.setLevel(7);
+//        GeneratorUnits generatorUnits = new GeneratorUnits(hero);
+//        List<Monster> listMonster = generatorUnits.generateMonsters();
+//
+//        System.out.println(listMonster);
+//        System.out.println(listMonster.size());
+//
+//        Combat combat = new Combat(hero,listMonster);
+//        combat.combat();
 
-        Combat combat = new Combat(hero,listMonster);
-        combat.combat();
+
+
     }
 }
--- src/units/Merchant.java
+++ src/units/Merchant.java
@@ -1,13 +1,44 @@
 package units;
 
-public class Merchant extends Unit{
+import java.util.List;
+
+public class Merchant extends Unit {
     public Merchant(String name, int health, int gold, int experience) {
         super(name, health, gold, experience);
     }
 
+    private final List<Goods> listOfGood = List.of(Goods.POTION25, Goods.POTION50, Goods.POTION100);
+
+    public List<Goods> getListOfGood() {
+        return listOfGood;
+    }
+
+    enum Goods {
+
+        POTION25(25, 25),
+        POTION50(50, 50),
+        POTION100(100, 100);
+        private final int point;
+        private final int price;
+
+        Goods(int point, int price) {
+            this.point = point;
+            this.price = price;
+        }
+
+        public int getPoint() {
+            return point;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
+
+
 
     @Override
-    public String toString(){
+    public String toString() {
         return name + ", HP = " + health + ", gold = " + gold;
     }
 }
